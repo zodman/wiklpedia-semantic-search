d92baf26e8b709d3d435970d4755c82d5c0515ea
zodman <zodman@gmail.com>
2023-05-18 14:39:46 -0400

    Add tests for crawler brainquotes and other scientists.

---
+++ tests/test_crawler.py
@@ -0,0 +1,5 @@
+import crawlers
+
+
+def test_crawler_brainquotes():
+    data = crawlers.crawler_quotes('My name doest not exists here aleluya')
---
+++ tests/test_other_scients.py
@@ -0,0 +1,15 @@
+import pytest
+import crawlers
+import formats
+
+
+@pytest.mark.parametrize('name', ['Euclid', 'Alan Turing'])
+def test_others(name):
+    data = crawlers.Scientistic.crawl(name)
+    assert 'title' in data and data['title'] != ''
+    assert 'summary' in data and data['summary'] != ''
+    assert 'born_date' in data
+    assert 'dead_date' in data
+    assert 'quote' in data
+    assert formats.to_text([data]) is None
+    assert formats.to_json([data]) is None
