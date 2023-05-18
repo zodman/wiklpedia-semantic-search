cf2efc8adcaf73dc62982b2f6ff54dc2d705f0d4
zodman <zodman@gmail.com>
2023-05-18 13:08:48 -0400

    finish unittest

--- constants.py
+++ constants.py
@@ -1,11 +1,12 @@
 import os
 
 SCIENTISTS = [
-    "Albert Einstein", "Isaac Newton", "Marie Curie", "Charles Darwin"
+    # "Albert Einstein", "Isaac Newton", "Marie Curie", "Charles Darwin"
+    'Thales',
+    'Ada Lovelace',
 ]
 HEADLESS = os.environ.get("HEADLESS")
 
 DATE_FORMAT = "%-d %B %Y"
 
 BASE_DIR = os.path.dirname(__file__)
-
